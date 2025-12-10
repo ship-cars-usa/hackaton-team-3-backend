ae8f695d1d8e4b1e9d393ba135336831737b74da
Milen Mihaylov <milen.mihaylov@ship.cars>
2025-12-10 12:41:09 +0200

    start app

---
+++ ai_damage_inspection/app/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
+class DamageInspectionView(ViewSet):
+    @action(detail=False, methods=['post'])
+    def damage_inspection(self, request):
+        return Response({"message": "Damage inspection endpoint"})
